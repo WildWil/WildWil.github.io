f0d9f44d83b895baa2b778b03e85ffefc4f973bf
William Ward <117488920+WildWil@users.noreply.github.com>
2023-08-31 15:05:30 -0400

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -1,16 +1,1 @@
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
 
-loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
-
-    if (username === "user" && password === "web_dev") {
-        alert("You have successfully logged in.");
-        window.location = '/FlowOS.html';
-    } else {
-        loginErrorMsg.style.opacity = 1;
-    }
-})
