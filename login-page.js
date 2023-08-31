61a792d01179b58229da4c970154409d212416c4
William Ward <117488920+WildWil@users.noreply.github.com>
2023-08-31 10:42:16 -0400

    When logged in will reload to FlowOS page

--- login-page.js
+++ login-page.js
@@ -9,7 +9,7 @@ loginButton.addEventListener("click", (e) => {
 
     if (username === "user" && password === "web_dev") {
         alert("You have successfully logged in.");
-        location.reload();
+        location.reload(FlowOS.html);
     } else {
         loginErrorMsg.style.opacity = 1;
     }
