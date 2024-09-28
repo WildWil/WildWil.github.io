09833caedaf25fae7680c2098e4398488d5486ee
William Ward <117488920+WildWil@users.noreply.github.com>
2024-09-28 16:22:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-document.querySelector('.button').addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector('#contact').scrollIntoView({
-        behavior: 'smooth'
-    });
-});
+
